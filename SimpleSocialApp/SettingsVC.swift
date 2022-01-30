dc4d537fbcbff956fa56644295d4515b1697d2b0
hamitseyrek <hamitseyrek@gmail.com>
2022-01-30 13:27:35 +0300

    adding some segue for tabor

---
+++ SimpleSocialApp/SettingsVC.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsVC.swift
+//  SimpleSocialApp
+//
+//  Created by Hamit Seyrek on 30.01.2022.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logOutClicked(_ sender: Any) {
+        print("selam")
+        performSegue(withIdentifier: "toViewControllerS", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
