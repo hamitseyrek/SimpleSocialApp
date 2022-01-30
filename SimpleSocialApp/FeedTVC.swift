daaab8470b091c5da0e2c5e4b9dffc187776f58e
hamitseyrek <hamitseyrek@gmail.com>
2022-01-30 19:55:03 +0300

    create custom cell with UITableViewCell

---
+++ SimpleSocialApp/FeedTVC.swift
@@ -0,0 +1,29 @@
+//
+//  FeedTVC.swift
+//  SimpleSocialApp
+//
+//  Created by Hamit Seyrek on 30.01.2022.
+//
+
+import UIKit
+
+class FeedTVC: UITableViewCell {
+
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var userEmailTextField: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func likebuttonClicked(_ sender: Any) {
+    }
+}
