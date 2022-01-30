809337f71d654e609bd63abbbca91a1e873bd321
hamitseyrek <hamitseyrek@gmail.com>
2022-01-30 20:10:35 +0300

    create custom cell design

--- SimpleSocialApp/FeedVC.swift
+++ SimpleSocialApp/FeedVC.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        //configure for tableView
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    //MARK: - Table View Configuration
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! FeedTVC
+        cell.userEmailTextField.text = "user@g.com"
+        cell.userImageView.image = UIImage(named: "selectImage")
+        cell.commentLabel.text = "comment"
+        return cell
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
 
     /*
     // MARK: - Navigation
