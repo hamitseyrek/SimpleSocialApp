88db60c4084a803bb603b727172f4ef6cee8b832
hamitseyrek <hamitseyrek@gmail.com>
2022-01-30 13:07:04 +0300

    add firebase with cocoa pods

--- SimpleSocialApp/ViewController.swift
+++ SimpleSocialApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVCS", sender: nil)
+    }
+    @IBAction func signOutClicked(_ sender: Any) {
+    }
 }
 
